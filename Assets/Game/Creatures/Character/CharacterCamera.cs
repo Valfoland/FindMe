cc84d432789541dad6a1ea6ae637cf6b36bde016
Valerii Folomeev <folomeev.valera98@gmail.com>
2022-07-25 01:45:06 +0600

    Update logic

--- Assets/Game/Creatures/Character/CharacterCamera.cs
+++ Assets/Game/Creatures/Character/CharacterCamera.cs
@@ -4,18 +4,45 @@ namespace Game.Creatures.Character
 {
     public class CharacterCamera : ICharacterCommand
     {
+        private const float RotationSpeed = 4f;
+        
+        private Vector3 _offset;
         private CharacterData _characterData;
+
+        private float _cameraRotationX;
+        private float _cameraRotationY;
+        private float _delay;
         
         public CharacterCamera(CharacterData characterData)
         {
             _characterData = characterData;
+            
+            _offset = _characterData.CharacterCameraObject.transform.localPosition;
+            _cameraRotationY = _characterData.CharacterCameraObject.transform.eulerAngles.y;
+            _cameraRotationX = _characterData.CharacterCameraObject.transform.eulerAngles.x;
         }
     
         public void DoAction()
         {
-            var characterPos = _characterData.CharacterObject.transform.position;
-            var pos = new Vector3(characterPos.x, characterPos.y + 5, characterPos.z);
-            _characterData.CharacterCameraObject.transform.position = pos;
+            MoveOrbital();
+        }
+
+        private void MoveOrbital()
+        {
+            _cameraRotationY += Input.GetAxis("Mouse X") * RotationSpeed;
+            _cameraRotationX -= Input.GetAxis("Mouse Y") * RotationSpeed;
+            _cameraRotationX = Mathf.Clamp(_cameraRotationX, -25, 25);
+
+            var characterTransform = _characterData.CharacterObject.transform;
+            var characterTransformPosition = characterTransform.position;
+            
+            var trs = Matrix4x4.TRS(characterTransform.localPosition, Quaternion.Euler(_cameraRotationX, _cameraRotationY, 0), characterTransform.localScale);
+            
+            _characterData.CharacterCameraObject.transform.position = characterTransformPosition + trs.MultiplyVector(_offset);
+
+            var lookPosition = new Vector3(characterTransformPosition.x, characterTransformPosition.y + 1, characterTransformPosition.z);
+            
+            _characterData.CharacterCameraObject.transform.LookAt(lookPosition);
         }
     }
 }
--- Assets/Game/Creatures/Character/CharacterTransform.cs
+++ Assets/Game/Creatures/Character/CharacterTransform.cs
@@ -5,7 +5,11 @@ namespace Game.Creatures.Character
 {
     public class CharacterTransform : ICharacterCommand
     {
+        private const float TurnSmoothTime = 0.05f;
+        
         private CharacterData _characterData;
+        private float velocity;
+        
         
         public CharacterTransform(CharacterData characterData)
         {
@@ -14,8 +18,18 @@ namespace Game.Creatures.Character
 
         public void DoAction()
         {
+            var direction = new Vector3(Input.GetAxis("Horizontal"), 0,  Input.GetAxis("Vertical"));
+
+            var angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _characterData.CharacterCameraObject.transform.eulerAngles.y;
+            var smoothAngle = Mathf.SmoothDampAngle(_characterData.transform.eulerAngles.y, angle, ref velocity, TurnSmoothTime);
+            
+            _characterData.transform.rotation = 
+                Quaternion.Euler(0, smoothAngle, 0);
+
+            var magnitude = Vector3.Magnitude(new Vector3(Input.GetAxis("Horizontal"), 0,  Input.GetAxis("Vertical")));
+
 #if UNITY_EDITOR
-            _characterData.Animator.SetFloat("BlendKoeff", Input.GetAxis("Vertical"));
+            _characterData.Animator.SetFloat("BlendKoeff", magnitude);
 #endif
         }
     }
